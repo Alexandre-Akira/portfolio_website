ac6dfcbbd72cbdde4ecab08c600572db929fe9a4
Alexandre Akira Enjiu <akira.enjiu@gmail.com>
2022-01-27 17:41:36 -0300

    Project started
    - Mobile HOME created

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,234 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <nav class="navigation">
+    <rounter-link to="/" class="navigation__logo"
+      ><img src="./assets/images/logo.svg" />
+    </rounter-link>
+    <a><img src="./assets/images/icons/hamburger.svg" /></a>
+    <!-- <router-link v-if="teste" to="/">HOME</router-link>
+    <router-link v-if="teste" to="/about">PORTFOLIO</router-link>
+    <router-link v-if="teste" to="/about">CONTACT ME</router-link> -->
+  </nav>
+  <router-view class="main" />
+  <footer class="contact">
+    <router-link class="contact__logo" to="/"></router-link>
+    <div class="contact__navLinks">
+      <router-link class="contact__navLink" to="/">HOME</router-link>
+      <router-link class="contact__navLink" to="/about">PORTFOLIO</router-link>
+      <router-link class="contact__navLink" to="/about">CONTACT ME</router-link>
+    </div>
+
+    <div class="contact__socialMedias">
+      <rounter-link to="/" class="contact__mediaLink"
+        ><img src="./assets/images/icons/github.svg" />
+      </rounter-link>
+      <rounter-link to="/" class="contact__mediaLink"
+        ><img src="./assets/images/icons/linkedin.svg" />
+      </rounter-link>
+      <rounter-link to="/" class="contact__mediaLink"
+        ><img src="./assets/images/icons/twitter.svg" />
+      </rounter-link>
+    </div>
+  </footer>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  /* FONTS IMPORT */
+  @import url("https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
-#nav {
-  padding: 30px;
-}
+  /* CSS RESET */
+  html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    box-sizing: border-box;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  :root {
+    /* PRIMARY COLORS */
+    --slightlyDesaturatedCyan: #5fb4a2;
+    --darkBlue: #203a4c;
+    --grayishDarkBlue: #33323d;
+    /* SECONDARY COLORS */
+    --veryLightGreyBG: #fafafa;
+    --lightGreyTextField: #eaeaeb;
+    --brightRedErrors: #f43030;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  /* HEADINGS  */
+
+  h1 {
+    font-family: "Ibarra Real Nova", serif;
+    font-weight: bold;
+    font-size: 50px;
+    line-height: 50px;
+    letter-spacing: -0.45px;
+  }
+
+  h2 {
+    font-family: "Ibarra Real Nova", serif;
+    font-weight: bold;
+    font-size: 40px;
+    line-height: 42px;
+    letter-spacing: -0.36px;
+  }
+
+  h3 {
+    font-family: "Ibarra Real Nova", serif;
+    font-weight: regular;
+    font-size: 32px;
+    line-height: 36px;
+    letter-spacing: -0.29px;
+  }
+
+  .body1 {
+    font-family: "Public Sans", sans-serif;
+    font-weight: regular;
+    font-size: 16px;
+    line-height: 30px;
+  }
+
+  .body2 {
+    font-family: "Public Sans", sans-serif;
+    font-weight: regular;
+    font-size: 15px;
+    line-height: 30px;
+  }
+  #app {
+  }
+
+  .navigation {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 32px 32px 40px;
+  }
+
+  .navigation a {
+  }
+
+  .navigation a.router-link-exact-active {
+    color: #42b983;
+  }
+
+  .main {
+    padding: 0 32px;
+  }
+  .contact {
+    margin-top: 80px;
+    padding: 56px 136px;
+    text-align: center;
+    background-color: var(--grayishDarkBlue);
+  }
+
+  .contact__logo {
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+    background-color: #ffffff;
+    mask: url(./assets/images/logo.svg) no-repeat center / contain;
+    -webkit-mask: url(./assets/images/logo.svg) no-repeat center / contain;
+    width: 61px;
+    height: 32px;
+  }
+
+  .contact__navLinks {
+    padding: 40px 0;
+  }
+
+  .contact__navLink {
+    display: block;
+    text-decoration: none;
+    color: white;
+  }
+
+  .contact__navLink:not(:last-child) {
+    padding-bottom: 32px;
+  }
+
+  .contact__mediaLink:not(:last-child) {
+    padding-right: 15px;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img
+      src="../assets/images/homepage/mobile/image-homepage-hero.jpg"
+      alt="hero image"
+    />
+    <h2>Hey, I’m Alexandre Akira and I love building beautiful websites</h2>
+
+    <div class="card">
+      <header class="card__header">
+        <img
+          class="card__image"
+          src="../assets/images/homepage/mobile/image-homepage-profile.jpg"
+          alt=""
+        />
+        <hr />
+        <h2 class="card__title">About Me</h2>
+      </header>
+      <main class="card__main">
+        <p class="card__description body1">
+          I’m a junior front-end developer looking for a new role in an exciting
+          company. I focus on writing accessible HTML, using modern CSS
+          practices and writing clean JavaScript. When writing JavaScript code,
+          I mostly use Vue, but I can adapt to whatever tools are required. I’m
+          based in São Paulo, BR, but I’m happy working remotely and have
+          experience in remote teams. When I’m not coding, you’ll find me
+          outdoors. I love being out in nature whether that’s going for a walk,
+          run or cycling. I’d love you to check out my work.
+        </p>
+      </main>
+
+      <footer class="card__footer">
+        <div class="card__link">
+          <rounter-link to="/">GO TO PORTFOLIO</rounter-link>
+        </div>
+      </footer>
+      <hr />
+
+      <div class="home__footer">
+        <h2>Interested in doing a project together?</h2>
+
+        <div class="card__link card__link--center">
+          <rounter-link to="/">CONTACT ME</rounter-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+  // @ is an alias to /src
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: "Home",
+    components: {},
+  };
 </script>
+
+<style scoped>
+  h2 {
+    padding-top: 24px;
+    padding-bottom: 32px;
+    color: var(--grayishDarkBlue);
+  }
+
+  .card__description {
+    color: var(--grayishDarkBlue);
+    padding-bottom: 24px;
+  }
+
+  .card__footer {
+  }
+
+  .card__link {
+    border: 1px solid var(--grayishDarkBlue);
+    text-align: center;
+    padding: 17px;
+    width: 202px;
+    font-family: "Public Sans", sans-serif;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: 2px;
+  }
+
+  .card__link--center {
+    margin-right: auto;
+    margin-left: auto;
+  }
+
+  .home__footer {
+    text-align: center;
+  }
+</style>
